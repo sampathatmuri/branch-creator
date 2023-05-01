110005bb647affa65f52c1d2a1458c9390b9ef52
Sampath Atmuri <sampathatmuri31@gmail.com>
2023-05-01 13:11:28 -0400

    Functionality create github branches via java

---
+++ src/main/java/com/github/githubbranchcreator/GitHubBranchCreatorApplication.java
@@ -0,0 +1,32 @@
+package com.github.githubbranchcreator;
+
+import io.micrometer.observation.Observation;
+import org.kohsuke.github.GHRef;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class GitHubBranchCreatorApplication {
+
+	private static final String GITHUB_ACCESS_TOKEN = "your-token";
+
+	public static void main(String[] args) throws IOException {
+
+		GitHub github = new GitHubBuilder().withOAuthToken(GITHUB_ACCESS_TOKEN).build();
+
+		String[] repositories = {"sampathatmuri/bookmarks","sampathatmuri/jhipster-microservice"};
+		for (String repo : repositories) {
+			GHRepository repository = github.getRepository(repo);
+			GHRef masterRef = repository.getRef("heads/master");
+			repository.createRef("refs/heads/my-new-branch-2", masterRef.getObject().getSha());
+			System.out.println("Branch created in " + repo);
+		}
+
+	}
+
+}
---
+++ src/test/java/com/github/githubbranchcreator/GitHubBranchCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.githubbranchcreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubBranchCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
